6254877efe7a73d3a2d3de3ca49e5cbbeb5f2e7a
yogi <yogesh21india@gmail.com>
2018-11-24 10:13:59 +0530

    complete demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { Contacts } from '@ionic-native/contacts';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -23,6 +24,7 @@ import { HomePage } from '../pages/home/home';
   ],
   providers: [
     StatusBar,
+    Contacts,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,69 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+import { Contacts, Contact } from '@ionic-native/contacts';
+import { parseNumber } from 'libphonenumber-js'
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  phone_no: any;
+  constructor(
+    public alertCtrl: AlertController,
+    private contacts: Contacts,
+    public navCtrl: NavController
+  ) {
 
-  constructor(public navCtrl: NavController) {
+    // this.getcontact();
+  }
 
+  getcontact() {
+    this.contacts.pickContact().then((res: Contact) => {
+      console.log("Get contact Success");
+      console.log(res);
+      console.log(res.phoneNumbers);
+      this.showRadio(res.phoneNumbers);
+    }).catch(err => {
+      console.log("Get contact Error");
+      console.log(err);
+    });
+  }
+
+
+  showRadio(list:any) {
+    let alert = this.alertCtrl.create();
+    alert.setTitle('Phone no to use');
+
+    for(var i=0; i<list.length; i++){
+      alert.addInput({
+        type: 'radio',
+        label: list[i].value,
+        value: list[i].value
+      });
+    }
+    
+    alert.addButton('Cancel');
+    alert.addButton({
+      text: 'OK',
+      handler: data => {
+        if(data != undefined){
+          console.log("DATA in btn ok");
+          console.log(data);
+          console.log("----------------------------------");
+          let res = parseNumber(data) ;
+          console.log(res.phone);
+          if(res.phone != undefined){
+            this.phone_no = res.phone;
+          }else{
+            this.phone_no = data;
+          }  
+        }else{
+          console.log("No number selected");
+        }
+      }
+    });
+    alert.present();
   }
 
 }
